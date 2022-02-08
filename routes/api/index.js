0d5c51fccd4a17fbc9a35eea7c6c952c2ad959df
mediazjr <mediazjr@gmail.com>
2022-02-08 13:16:17 -0800

    added user and thought routes and required them in index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 
+const thoughtRoutes = require("./thoughtRoutes");
+const userRoutes = require("./userRoutes");
 
+
+router.use("/thought", thoughtRoutes);
+router.use("/user", userRoutes);
 
 
 
