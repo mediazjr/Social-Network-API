03c0540646a468a95b43a9a1ad4ce4a78b75decb
mediazjr <mediazjr@gmail.com>
2022-02-08 14:12:54 -0800

    added update user function to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,7 +35,43 @@ module.exports = {
             .catch((err) => res.status(500).json(err))
     },
 // update
-
-
-
-}
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+            !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+        )
+            .catch((err) => res.status(500).json(err));
+    },
+    addFriend(req,res){
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: {friends:req.params.friendId}},
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+            !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+        )
+            .catch((err) => res.status(500).json(err));
+    },
+    removeFriend(res,req){
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: {friends:req.params.friendId}},
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+            !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+        )
+            .catch((err) => res.status(500).json(err));
+    }
+};
